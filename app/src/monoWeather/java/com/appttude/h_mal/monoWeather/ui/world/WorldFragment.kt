2632be938a0f3b73aa9eb8b1d95943e4c5bdde6c
Haider Malik <h.malik144@gmail.com>
2024-10-06 13:40:46 +0100

    latest mono release (#42)
    * Imperial units (#24)
    * Update build.gradle (#26)
    * - fix db bug (#28)
     - upgrade application to android 34
    * Api fix (#38)
    * Playstore review fix (#41)

--- app/src/monoWeather/java/com/appttude/h_mal/monoWeather/ui/world/WorldFragment.kt
+++ app/src/monoWeather/java/com/appttude/h_mal/monoWeather/ui/world/WorldFragment.kt
@@ -2,7 +2,6 @@ package com.appttude.h_mal.monoWeather.ui.world
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
